e4ed931caea045bb773421c723bd9ee5ec105b09
veektrie <102025110+veektrie@users.noreply.github.com>
2022-12-01 03:53:06 +0100

    imports for explore page completed

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,9 +1,13 @@
-'use client';
+"use client";
+import { useState } from "react";
 
-const Explore = () => (
-  <section>
-    Explore section
-  </section>
-);
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { ExploreCard, TitleText, TypingText } from "../components";
+import { exploreWorlds } from "../constants";
+
+const Explore = () => <section>Explore section</section>;
 
 export default Explore;
